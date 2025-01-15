5b2d437eb44fb0334c81b0e742210809480dc273
Allix Dolou <dolou.allix@gmail.com>
2025-01-15 18:01:37 +0100

    Ajout de la bannière

--- kasa/src/pages/Home/index.jsx
+++ kasa/src/pages/Home/index.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import '../Home'; 
 
 export default function Home() {
-    return null;
-  }
+  return (
+    <div className="banner-elements">
+      <div className="banner">
+        <img src="/src/assets/images/Imagesource1.png" alt="Bannière" className="banner-image"/>
+        <div className="banner-overlay"></div>
+        <h1 className="banner-title">Chez vous, partout et ailleurs</h1>
+      </div>
+    </div>
+  );
+}
+
   
