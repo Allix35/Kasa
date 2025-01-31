47fd68928a925c5396b93b2ec27ed89fb668c757
Allix Dolou <dolou.allix@gmail.com>
2025-01-31 10:02:00 +0100

    Ajout de la page not found version mobile

--- kasa/src/pages/NotFound/index.jsx
+++ kasa/src/pages/NotFound/index.jsx
@@ -1,13 +1,14 @@
-
 import { Link } from 'react-router-dom';
 import '../../styles/pages/notfound.scss';
 
 const NotFound = () => {
   return (
     <div className="notfound-page">
-      <div className="nofound-content">
+      <div className="notfound-content">
         <h1 className="notfound-title">404</h1>
-        <p className="notfound-text"><p>Oops! La page que vous demandez n&apos;existe pas.</p>
+        <p className="notfound-text">
+          <span className="notfound-text-line">Oops! La page que</span> 
+          <span className="notfound-text-line"> vous demandez n&apos;existe pas.</span>
         </p>
         <Link to="/" className="notfound-return-link">Retourner sur la page d&apos;accueil</Link>
       </div>
@@ -16,5 +17,6 @@ const NotFound = () => {
 };
 
 export default NotFound;
+
 
   
